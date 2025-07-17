f0833aa871367d6a25c0ffb50718714d5d7799c4
opatel2426 <om.patel24265@gmail.com>
2025-07-16 20:04:20 -0500

    BCA_with_Normalization_and_Click_Reaction_Gel.py

--- BCA_with_Normalization_and_Click_Reaction_Gel_Tom.py
+++ BCA_with_Normalization_and_Click_Reaction_Gel_Tom.py
@@ -47,7 +47,7 @@ def run(protocol: protocol_api.ProtocolContext):
     heater_shaker.open_labware_latch()
 
     #set the temp module to 0c
-    temp_module.set_temperature(celsius=4)
+    temp_module.set_temperature(celsius=10)
     
     # Load labware
     partial_50 = protocol.load_labware(load_name="opentrons_flex_96_filtertiprack_50ul",location="B3")
